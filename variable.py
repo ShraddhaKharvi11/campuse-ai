c76a4286f4859f6e76d5a6626e83a2e723aa3e97
ShraddhaKharvi11 <shraddhakharvi2228@gmail.com>
2026-02-23 14:36:50 +0530

    Variable

---
+++ variable.py
@@ -0,0 +1,23 @@
+#Allow
+myage=21
+my_age=21
+_my_age=21
+myAge=21
+MYAGE=21
+myage1=21
+
+#Worng/illegal
+#1myage=21
+#my-var=10
+#my var=11
+
+#assign variables
+a,b,c="Dennis","Manjunath","sneha"
+print(a)
+print(b)
+print(c)
+
+d=e=f="Dennis",12,"You"
+print(d)
+print(e)
+print(f)
