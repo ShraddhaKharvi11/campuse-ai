df894414117fac32017095aba8525c00e4ea4bf5
ShraddhaKharvi11 <shraddhakharvi2228@gmail.com>
2026-02-24 23:18:07 +0530

    Added Conditional statement

---
+++ conditional-statements.py
@@ -0,0 +1,51 @@
+num_str=input("Hi what is your name ?")
+age=int(input("Hi "+num_str+", What is your name?"))
+
+password='123123123'
+
+if password=="secreate@123":
+    print("Access Granted")
+else:
+    print("Access Denied")
+
+if age>=18:
+    print("You are a adult")
+    print("You can vote")
+
+else:
+    print("You are a baccha")
+    print("Go have some ice cream")
+
+
+#if elif statement
+score=85
+
+if score>=90:
+    grade='A'
+elif score>=80:
+    grade='B'
+elif score>=70:
+    grade='C'
+elif score>=60:
+    grade='D'
+else:
+    grade='F'
+
+print("Your grade is:",grade)
+
+#Nested if else
+
+age=25
+has_license=True
+
+if age>=18:
+    print("Your are old enough to drive")
+
+    if has_license:
+        print("You can Drive!")
+    
+    else:
+        print("You need a License")
+
+else:
+    print("Too Young to Drive")
