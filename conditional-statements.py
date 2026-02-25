31ed49e4b8d25461990e7ef13078f66ce918824c
ShraddhaKharvi11 <shraddhakharvi2228@gmail.com>
2026-02-25 23:54:19 +0530

    Added Functions and Looping statements

--- conditional-statements.py
+++ conditional-statements.py
@@ -1,5 +1,5 @@
 num_str=input("Hi what is your name ?")
-age=int(input("Hi "+num_str+", What is your name?"))
+age=int(input("Hi "+num_str+", What is your age?"))
 
 password='123123123'
 
@@ -49,3 +49,39 @@ if age>=18:
 
 else:
     print("Too Young to Drive")
+
+
+# if-else using AND condition
+
+age=20
+has_ticket=True
+
+if age>=18 and has_ticket:
+    print("You can enter the concert")
+
+else:
+    print("Cannot enter")
+
+#if-else using OR statement
+
+day="Saturday"
+
+if day=="Sunday" or day=="Saturady":
+    print("It is a weekend")
+
+else:
+    print("It is a weekday")
+
+#if-else using Not
+
+is_sunny=True
+
+if not is_sunny:
+    print("its Sunny")
+else:
+    print("its cloudy")
+
+#Ternary Operator
+
+status='adlut' if age>=18 else 'minor'
+print(status)
